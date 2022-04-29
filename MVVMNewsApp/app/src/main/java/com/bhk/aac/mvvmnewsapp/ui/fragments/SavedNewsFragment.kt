d002e6edf23492928459d5b9bd33ff295bfd7ca6
hankim <42214405+byhankim@users.noreply.github.com>
2022-04-29 16:22:07 +0900

    Refact: add the viewmodel from main activity on each fragment

--- MVVMNewsApp/app/src/main/java/com/bhk/aac/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
+++ MVVMNewsApp/app/src/main/java/com/bhk/aac/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
@@ -1,8 +1,18 @@
 package com.bhk.aac.mvvmnewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.bhk.aac.mvvmnewsapp.R
+import com.bhk.aac.mvvmnewsapp.ui.NewsActivity
+import com.bhk.aac.mvvmnewsapp.ui.NewsViewModel
 
 class SavedNewsFragment: Fragment(R.layout.fragment_saved_news) {
 
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+    }
 }
