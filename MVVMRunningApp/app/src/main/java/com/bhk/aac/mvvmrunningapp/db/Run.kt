7ed622ab59053e1645ea0b35cc80b1505ff07853
hankim <42214405+byhankim@users.noreply.github.com>
2022-05-11 04:00:03 +0900

    Add: Entity object 4 room

---
+++ MVVMRunningApp/app/src/main/java/com/bhk/aac/mvvmrunningapp/db/Run.kt
@@ -0,0 +1,19 @@
+package com.bhk.aac.mvvmrunningapp.db
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "running_table")
+data class Run(
+    var img: Bitmap? = null,
+    var timestamp: Long = 0L, // when the run was, millis, easy to sort by date
+    var avgSpeedInKMH: Float = 0F,
+    var distanceInMegers: Int = 0,
+    var timeInMillis: Long = 0L, // how long the run was
+    var caloriesBurnt: Int = 0
+) {
+    // let Room create PK
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
