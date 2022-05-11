9b7b4fcf14d4557693e104aa16f73d8243b34f62
hankim <42214405+byhankim@users.noreply.github.com>
2022-05-11 10:38:17 +0900

    Fix: hilt gradle error solved

--- MVVMRunningApp/app/src/main/java/com/bhk/aac/mvvmrunningapp/db/RunningDatabase.kt
+++ MVVMRunningApp/app/src/main/java/com/bhk/aac/mvvmrunningapp/db/RunningDatabase.kt
@@ -7,7 +7,8 @@ import androidx.room.TypeConverters
 // Actual class for the DB
 @Database(
     entities = [Run::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class RunningDatabase: RoomDatabase() {
