e9f12ab3aae903b0decdee5036887039a0795cb0
hankim <42214405+byhankim@users.noreply.github.com>
2022-05-13 11:24:49 +0900

    Refact: move mainactivity to ui pkg

--- MVVMRunningApp/app/src/main/java/com/bhk/aac/mvvmrunningapp/ui/MainActivity.kt
+++ MVVMRunningApp/app/src/main/java/com/bhk/aac/mvvmrunningapp/ui/MainActivity.kt
@@ -2,16 +2,26 @@ package com.bhk.aac.mvvmrunningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.bhk.aac.mvvmrunningapp.databinding.ActivityMainBinding
+import com.bhk.aac.mvvmrunningapp.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    @Inject
+    lateinit var runDAO: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
         }
+
+        // test RunDao
+        Log.e("MAINACTIVITY", "rundao: ${runDAO.hashCode()}")
     }
 }
